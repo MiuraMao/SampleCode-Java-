dbe14ed7d3c0d8df7aba036313664c89adc5144e
MiuraMao <m_miura06@outlook.com>
2021-02-27 15:10:27 +0900

    整数和

---
+++ 整数和.java
@@ -0,0 +1,8 @@
+class 整数和 {
+    public static void main(String[] args){
+        int 合計=0;
+        for (int 数=1; 数<=10; 数++)
+            合計=合計+数;
+        System.out.printf("合計=%d\n", 合計);
+    }    
+}
