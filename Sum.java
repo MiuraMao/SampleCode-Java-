4e8f08d4b86b89cf311bad63ec0bd30c1eed1dc6
MiuraMao <m_miura06@outlook.com>
2021-02-27 14:57:21 +0900

    Sum

---
+++ Sum.java
@@ -0,0 +1,8 @@
+class Sum {
+    public static void main(String[] astgs) {
+        int total=0;
+        for (int i=1; i<=10; i++)
+            total = total+i;
+        System.out.printf("total=%d\n", total);
+    }
+}
