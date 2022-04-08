0277b44cf2c7c0e145ccbef07366efc2f1f1ecc4
MiuraMao <m_miura06@outlook.com>
2022-04-08 17:17:32 +0900

    ClassMethod

---
+++ ClassMethod.java
@@ -0,0 +1,19 @@
+//クラスメソッドのユーザー定義
+class Involution {
+    public static int square(int i) {   //クラスフィールドと同様にstaticをつける
+        return i*i;
+    }
+    public static int cube(int i) {     //クラスフィールドと同様にstaticをつける
+        return i*i*i;
+    }
+}
+
+class ClassMethod {
+    public static void main(String[] args) {
+        int i=5, j;
+        j=Involution.square(i);
+        System.out.printf("%d × %d=%d\n",i,i,j);
+        j=Involution.cube(i);
+        System.out.printf("%d × %d × %d=%d\n",i,i,i,j);
+    }
+}
