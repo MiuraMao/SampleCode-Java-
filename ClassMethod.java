a9e4296a0dc65f7cb8868727aeca95a581139648
MiuraMao <m_miura06@outlook.com>
2022-04-08 17:18:46 +0900

    ClassMethod

--- ClassMethod.java
+++ ClassMethod.java
@@ -9,7 +9,7 @@ class Involution {
 }
 
 class ClassMethod {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //mainもクラスメソッド
         int i=5, j;
         j=Involution.square(i);
         System.out.printf("%d × %d=%d\n",i,i,j);
