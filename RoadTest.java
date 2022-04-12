54eca04d19a791eb6e8ceb5d730a2b17893f71f1
MiuraMao <m_miura06@outlook.com>
2022-04-12 16:45:35 +0900

    RoadTest

--- RoadTest.java
+++ RoadTest.java
@@ -2,7 +2,7 @@
 class Road extends Polyline {
     String name;
     Road(int n, int[] xx, int[] yy, String s) {
-        super(n,xx,yy);     //スーパークラスの呼び出し（super、デフォルトコンストラクタが必ず呼び出される）
+        super(n,xx,yy);     //スーパークラスの呼び出し（super、コンストラクタ定義の先頭に記述、デフォルトコンストラクタが必ず呼び出される）
         name=new String(s); //独自に追加された情報name
     }
 
