2af2baa97a66191737673fd5434fa5d6f0a661db
MiuraMao <m_miura06@outlook.com>
2022-04-19 21:35:22 +0900

    Object1

--- Object1.java
+++ Object1.java
@@ -4,7 +4,7 @@ class A {
     public String toString() {
     return "("+i+") :"+s;        
     }
-    public boolean equals(A obj) {
+    public boolean equals(A obj) {              //equalsメソッドをオーバーライド
         if(this.i==obj.i && this.s.equals(obj.s)) return true;
         else return false;
     }
@@ -13,19 +13,19 @@ class Object1 {
     public static void main(String [] args) {
         A ins=new A(); ins.i=10; ins.s="abcdefg";
         A cpy=ins;
-        System.out.println(ins.getClass());
-        System.out.println(ins.toString());
-        System.out.println(ins);
+        System.out.println(ins.getClass());     //関連づけられているオブジェクトが属すクラスを返す
+        System.out.println(ins.toString());     //オブジェクトの内容を文字列変換
+        System.out.println(ins);                //toStringを省略しても暗黙的に呼び出される
         A oth=new A(); oth.i=10; oth.s="abcdefg";
-        if(ins == cpy)
+        if(ins == cpy)      //同一オブジェクトのため真（値の比較ではない）
             System.out.println("ins と cpy は等しい");
         else
             System.out.println("ins と cpy は異なる");
-        if(ins == oth)
+        if(ins == oth)      //値は同じだが異なるオブジェクトのため偽
             System.out.println("ins と oth は等しい");
         else
             System.out.println("ins と oth は異なる");
-        if(ins.equals(oth))
+        if(ins.equals(oth)) //内容の真偽判定
             System.out.println("ins と oth は内容が同じ");
         else
             System.out.println("ins と oth は内容が異なる");
