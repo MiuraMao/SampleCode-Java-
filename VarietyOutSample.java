cfe873b0843db0b5302d269aaf238833f9071bc9
MiuraMao <m_miura06@outlook.com>
2022-04-21 18:40:20 +0900

    VarietyOutSample

---
+++ VarietyOutSample.java
@@ -0,0 +1,37 @@
+interface VarietyOut {
+    static void print(String msg) {
+        System.out.println("print:"+msg);
+    }
+    default void disp(String msg) {
+        System.out.println("disp:"+msg);
+    }
+    void output(String msg);
+}
+
+class VOCA implements VarietyOut {
+    public void output(String msg) {
+        System.out.println("outputA:"+msg);
+    }
+}
+
+class VOCB implements VarietyOut {
+    public void output(String msg) {
+        System.out.println("outputB:"+msg.toUpperCase());
+    }
+    public void disp(String msg) {
+        StringBuffer sb=new StringBuffer(msg);
+        System.out.println("dispB:"+sb.reverse());
+    }
+}
+
+class VarietyOutSample {
+    public static void main(String[] args) {
+        VOCA voca=new VOCA();
+        VOCB vocb=new VOCB();
+        VarietyOut.print("‚ ‚¢‚¤");
+        voca.disp("123");
+        voca.output("abc");
+        vocb.disp("123");
+        vocb.output("abc");
+    }
+}
