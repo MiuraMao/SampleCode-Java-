acfff7afda24441b01086c05f78a2137ef79e23b
MiuraMao <m_miura06@outlook.com>
2022-04-22 21:09:37 +0900

    AnonymousClass

--- AnonymousClass.java
+++ AnonymousClass.java
@@ -9,7 +9,7 @@ class AnonymousClass {      //匿名クラス（無名内部クラス）
         ary[1]=new Person("鈴木　一郎","すずき　いちろう",21);
         ary[2]=new Person("藤原　晋","ふじわら　すすみ",20);
         ary[3]=new Person("源氏　陽子","げんじ　ようこ",22);
-        Array.sort(ary,new Comparator<Person>()  {      //Array.sortの第２引数にインターフェイスであるComparatorを指定
+        Arrays.sort(ary,new Comparator<Person>()  {      //Array.sortの第２引数にインターフェイスであるComparatorを指定
             public int compare(Person a, Person b) {
                 return a.yomi.compareTo(b.yomi);
             }
