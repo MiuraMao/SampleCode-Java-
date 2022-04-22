e9cdb84fd1a076cc5851a09d59ed284dd30a921c
MiuraMao <m_miura06@outlook.com>
2022-04-22 20:37:08 +0900

    ArraysSample

--- ArraysSample.java
+++ ArraysSample.java
@@ -11,7 +11,7 @@ class ArraysSample {
         int[] ary={8,4,7,2,5,1};
         System.out.println("Before Sort");
         printAry(ary);
-        Arrays.sort(ary);
+        Arrays.sort(ary);       //オブジェクト間の大小比較を行う方法をArraysクラスに教える必要がある
         System.out.println("After Sort");
         printAry(ary);
     }
