1694f240bb6844566c495e51fa4f2de7cc944857
MiuraMao <m_miura06@outlook.com>
2022-04-24 17:38:50 +0900

    Except

---
+++ Except.java
@@ -0,0 +1,8 @@
+//Exception‚Ì”­¶
+class Except {
+    public static void main(String[] args) {
+        int[] ary=new int[5];
+        ary[8]=5;
+        System.out.println("ary[8]‚É5‚ğ‘ã“ü‚µ‚½");
+    }    
+}
