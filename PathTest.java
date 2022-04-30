9912b6158b3847e4b9828e06cb5aeed1aa39b09f
MiuraMao <m_miura06@outlook.com>
2022-04-30 22:51:30 +0900

    PathTest

--- PathTest.java
+++ PathTest.java
@@ -2,13 +2,13 @@ import java.nio.file.*;
 class PathTest {
     public static void main(String[] args) {
         if(args.length==0) {
-            System.out.println("Use: java PathTest <file-name>");
+            System.out.println("Use: java PathTest <file-name>");   //ファイル名をコマンド引数で指定
             System.exit(0);
         }
-        Path p=Paths.get(args[0]);
+        Path p=Paths.get(args[0]);      //コマンド引数を受け取る
         Path ap=p.toAbsolutePath();
         System.out.println("Absolute Path="+ap);
-        int nc=ap.getNameCount();
+        int nc=ap.getNameCount();       //パスのルートから何段フォルダをたどるかカウント
         System.out.println("Name Count="+nc);
         Path pp=ap.getParent();
         System.out.println("Parent Path="+pp);
