f08cd234598f4f4ad3dc22bb8c5922faf9b3d945
MiuraMao <m_miura06@outlook.com>
2022-04-30 22:57:51 +0900

    FileTest

---
+++ FileTest.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+class FileTest {
+    public static void main(String[] args) throws Exception{
+        Path p=Paths.get("test.txt");
+        FileTime ft=Files.getLastModifiedTime(p);
+        System.out.println("Create time="+ft);
+        System.out.println("File? "+Files.isRegularFile(p));
+        System.out.println("Directory? "+Files.isDirectory(p));
+        System.out.println("Size: "+Files.size(p));
+        Path newPath=Paths.get("testCopy.txt");
+        Files.copy(p,newPath);
+    }
+}
