89669359e016679a510634dfb227b34b197248d1
MiuraMao <m_miura06@outlook.com>
2022-04-30 22:12:59 +0900

    FromFile

--- FromFile.java
+++ FromFile.java
@@ -4,8 +4,8 @@ class FromFile {
     public static void main(String[] args) {
         int i,j,k;
         try {
-            Path path=Paths.get("test.txt");
-            Scanner kin=new Scanner(path);
+            Path path=Paths.get("test.txt");  //例外を発生させる可能性がある
+            Scanner kin=new Scanner(path);          //Scanner型のオブジェクト作成時にPath型の引数を渡す
             i=kin.nextInt(); System.out.println("i="+i);
             j=kin.nextInt(); System.out.println("j="+j);
             k=i+j;
@@ -16,3 +16,6 @@ class FromFile {
         }
     }
 }
+/*区切り文字の変更*/
+//kin.useDelimiter("\\s*,\\s*");    //カンマを区切り文字に指定（前後の0個以上の空白文字を含む）
+//kin.userDelimiter("\\r\\n");      //Windowsの改行を区切り文字に指定
