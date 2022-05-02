6ae6f47684ce15a4498fda36d9d4364b66c0e333
MiuraMao <m_miura06@outlook.com>
2022-05-02 16:35:03 +0900

    Elapse

---
+++ Elapse.java
@@ -0,0 +1,15 @@
+//ソートの実行時間を計測
+import java.util.Arrays;
+class Elapse {
+    public static void main(String[] args) {
+        final int DATA_NUMBER=100000;
+        double ary[]=new double[DATA_NUMBER];
+        for(int i=0; i<DATA_NUMBER; i++)
+            ary[i]=Math.random();
+        long start=System.currentTimeMillis();      //現在時刻を返す
+        Arrays.sort(ary);
+        long end=System.currentTimeMillis();
+        System.out.println("time="+(end-start)+" ms"); 
+
+    } 
+}
