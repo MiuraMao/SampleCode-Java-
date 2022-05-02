eca1e0d36406f24f37e921e72ac2c849259b5fa7
MiuraMao <m_miura06@outlook.com>
2022-05-02 16:40:58 +0900

    Elapse

--- Elapse.java
+++ Elapse.java
@@ -6,10 +6,13 @@ class Elapse {
         double ary[]=new double[DATA_NUMBER];
         for(int i=0; i<DATA_NUMBER; i++)
             ary[i]=Math.random();
-        long start=System.currentTimeMillis();      //現在時刻を返す
+        long start=System.currentTimeMillis();      //現在時刻を返す（グリニッジ標準時1970年1月1日0時0分0秒）
         Arrays.sort(ary);
-        long end=System.currentTimeMillis();
+        long end=System.currentTimeMillis();        //Instant.now()でも取得できる
         System.out.println("time="+(end-start)+" ms"); 
 
     } 
 }
+//ナノ秒で測定する場合
+//long System.nanoTime()
+
